f47f5ffbe029e5e893bac2ea2b19a824598037e3
Murilo_Giacomini <giacomini.murilo@gmail.com>
2024-11-30 17:06:35 -0300

    Update and rename CMA.py to Model_test.py

--- Model_test.py
+++ Model_test.py
@@ -1,61 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  4 22:24:39 2024
-
-@author: Murilo
+Model_test.py
 """
 
 import pandas as pd
 import matplotlib.pylab as plt
 import numpy as np
 
-df = pd.read_csv(r"G7.csv", sep = ';', encoding='ISO-8859-1', usecols = [2,5])
+df = pd.read_csv(r"G7.csv", sep = ';', encoding='ISO-8859-1', usecols = [2,5]) # Reading csv file with the data, force and deformation
 df = df.iloc[1:]
 
-
+# Data processing
 df['Força'] = df['Força'].astype(str).str.replace('.', '', regex=False).str.replace(',', '.', regex=False)
 df['Deformação à tração (Deformação)'] = df['Deformação à tração (Deformação)'].astype(str).str.replace('.', '', regex=False).str.replace(',', '.', regex=False)
 df['Força'] = pd.to_numeric(df['Força'], errors='coerce')
 df['Deformação à tração (Deformação)'] = pd.to_numeric(df['Deformação à tração (Deformação)'], errors='coerce')
 
+# Unpack
 Kforca = np.array(df['Força'])
 defor = np.array(df['Deformação à tração (Deformação)'])
 
-
-forca = Kforca * 1000 # deixa a forca em N ao inves de kN
-tensao = forca / 0.000032578 #calcula a tensao sigma = F/A, A = (2.6 * 12.53 * 10^-6) m
+# Unit conversion
+forca = Kforca * 1000 # leaves the force in N instead of kN
+tensao = forca / 0.000032578 # calculates the stress sigma = F/A, A = (2.6 * 12.53 * 10^-6) m
 
 E = (tensao[400] / (defor[400]*1E-2)) # modulo de elasticidade (rigidez)
 
-#Máximos
+# Maximum point
 defor_max = defor[7327]
-tensao_max = tensao.max() # tensao maxima
+tensao_max = tensao.max() # stress maximum
 
-#Ruptura
+# Stress of rupture point
 defor_rup = defor[-1]
-tensao_rup = tensao[-1] # tensao de ruptura
+tensao_rup = tensao[-1] # Stress of rupture
 
-#Tenacidade
+# Tenacity
 area = np.trapz(tensao, defor/100)
 
+# Yield stress point 
 defor_escoamento = defor[819]
 tensao_escoamento = tensao[819]
 
-#def o tamanho da figura
+# Defines the size of the figure
 plt.figure(figsize=(25, 10))
 
 plt.scatter(defor_escoamento, tensao_escoamento, color='red', s=25, label='Ponto de máximo')
-#Máximo
+# Maximum
 plt.scatter(defor_max, tensao_max, color='red', s=25, label='Ponto de máximo')
-#Falha
+# Failure
 plt.scatter(defor_rup, tensao_rup, color='red', s=25, label='Ponto de falha')
 
-#Reta
-# Define a equação da reta
+# line
+# Define the equation of the line
 x = [0.2, defor[np.where(tensao >= tensao_escoamento)][0]]
-y = [0, tensao_escoamento]  # Usando o valor de young que você calculou anteriormente
+y = [0, tensao_escoamento]  # Using the yield stress value you calculated previously
 
-# Calcula a área sob o segmento de reta
+# Calculates the area under the line segment
 base = x[1]
 altura = tensao_escoamento
 area_segmento = (base * altura) / 2
@@ -63,12 +63,12 @@ area_segmento = (base * altura) / 2
 print("Área sob o segmento de reta:", area_segmento)
 
 
-# Plote a reta
+# Plot the line
 plt.plot(x, y, "b--", label="Reta")
-#Gráfico
+# Graphic
 plt.plot(defor, tensao, "r",label = "Gráfico")
 
-#Gráfico
+# Graphic
 
 plt.xticks(np.arange(0, defor.max() + 5, 1))
 plt.xlabel("Deformação (%)", fontsize = 14)
@@ -78,6 +78,7 @@ plt.title("Gráfico Tensão x Deformação", fontsize = 20)
 plt.legend()
 plt.show()
 
+# Print of the main values with your units
 print("Rigidez: ",E*1E-9, "GPa")
 print("Tensão máxima: ",tensao_max*1E-6, "MPa")
 print("Tenacidade: ",area*1E-6, "MJ/m^3 ou MPa")
